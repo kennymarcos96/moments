e225942c67469072e5369b067bd7b7b59f5e152b
Kenny <kennymarcos96@gmail.com>
2022-10-09 23:47:16 -0300

    header e footer ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'footer', component: FooterComponent},
+  {path: 'moments/new', component: NewMomentComponent},
+   // router redirect 404 error
+  { path: '**', redirectTo: '' }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
