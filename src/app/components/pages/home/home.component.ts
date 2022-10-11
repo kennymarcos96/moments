5b8aa7f2e19b10bb19d1fd24521962e7ff5e7741
Kenny <kennymarcos96@gmail.com>
2022-10-11 10:47:46 -0300

    API OK

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/moment';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl //Buscar dados API OLHAR Depois
+
+  //todo search
+
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
-  }
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
 
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+
+      this.allMoments = items.data;
+      this.moments = items.data;
+
+
+    });
+  }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Moment } from 'src/app/moment';
+import { environment } from 'src/environments/environment';
+import { Response } from 'src/app/Response';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MomentService {
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
+
+  createMoment(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData);
+  }
 }
